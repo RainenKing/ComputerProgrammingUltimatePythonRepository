ca7d789dbc39880fc9f34f5554a4475c5dd45246
Rainen King <27rainenking@usd266.com>
2023-12-12 09:11:00 -0600

    need to do vending machine on l 8

--- 08WhileLoopsForUserInput/Assignment/main.py
+++ 08WhileLoopsForUserInput/Assignment/main.py
@@ -1,31 +1,38 @@
 import random
 
-number = random.randint(0,10)
-response = int(input("enter a number"))
-while response < number:
-    print("to low")
-    response = int(input("enter a number"))
-while response > number:
-    print("to high")
-    response = int(input("enter a number"))
-else:
-    print("correct")
+#number = random.randint(0,10)
+#response = int(input("enter a number"))
+#while response < number:
+    #print("to low")
+    #response = int(input("enter a number"))
+#while response > number:
+    #print("to high")
+#    response = int(input("enter a number"))
+#else:
+#    print("correct")
 
 
-number = random.randint(0,10)
-lives = 2
-response = int(input("enter a number"))
-while response < number and lives > 0:
-    print(lives, "lives, to low")
-    response = int(input("enter a number"))
-    lives = lives - 1
-while response > number and lives > 0:
-    print(lives, "lives, to high")
-    response = int(input("enter a number"))
-    lives = lives - 1
+#number = random.randint(0,10)
+#lives = 2
+#response = int(input("enter a number"))
+#while response < number and lives > 0:
+#    print(lives, "lives, to low")
+#    response = int(input("enter a number"))
+#    lives = lives - 1
+#while response > number and lives > 0:
+#    print(lives, "lives, to high")
+#    response = int(input("enter a number"))
+#    lives = lives - 1
 
 
-if lives == 0:
-    print("out of lives you lose")
-else:
-    print("correct")
+#if lives == 0:
+#   print("out of lives you lose")
+#else:
+#    print("correct")
+
+
+
+number = 50
+print(number)
+amount = int(input("enter coin amount 5, 10, or 25"))
+total = number - amount
