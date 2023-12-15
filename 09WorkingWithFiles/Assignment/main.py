8e0228df82042264a35d1e3cb9bac41bd41a5f61
Rainen King <27rainenking@usd266.com>
2023-12-15 10:11:41 -0600

    done with the first one

--- 09WorkingWithFiles/Assignment/main.py
+++ 09WorkingWithFiles/Assignment/main.py
@@ -1,15 +1,31 @@
 import csv
+import json
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 f = open("../data/4000-most-common-english-words.csv", "r")
 words = f.read().split("\n")
-f.close()
 
-def most_vowels(words):
-    total = 0
-    for word in words:
-        if word in "aeiou":
-            total = total + 1
-    return total
 
-print(most_vowels(words))
 
+def find_most_vowels(wordlist):
+    most_so_far = ""
+    max_vowelcount = 0
+
+    for word in wordlist:
+        current_vowelcount = 0
+        for letter in word:
+            if letter== "a" or letter== "e" or letter== "i" or letter== "o" or letter== "u":
+                current_vowelcount = current_vowelcount + 1
+        if current_vowelcount > max_vowelcount:
+            max_vowelcount = current_vowelcount
+            most_so_far = word
+    return most_so_far
+
+print(find_most_vowels(words))
+
+
+
+def average_word_legth(wordlist):
